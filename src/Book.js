0819d591c8d1d89889409baadc848d7a313c7c8c
Sensei-Noah <98984612+Sensei-Noah@users.noreply.github.com>
2022-03-02 14:33:05 +0200

    master

---
+++ src/Book.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Book() {
+  return(
+    <div className="book">
+      <h2>Nice book</h2>
+      <img src="https://i.kym-cdn.com/entries/icons/facebook/000/026/152/gigachad.jpg" className="icon" />
+    </div>
+  )
+}
+export default Book;
---
+++ src/Items.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Book from "./Book"
+
+function Items() {
+  return(
+    <div className="list">
+      <Book />
+      <Book />
+      <Book />
+      
+    </div>
+  )
+}
+export default Items;
